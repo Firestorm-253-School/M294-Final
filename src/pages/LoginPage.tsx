16bf841301e39b20977b2bacd56c7e7ce4524483
Janick-aeschlimann <janick.aeschlimann@bbz-sh.ch>
2024-12-05 10:08:18 +0100

    login implement api

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,46 @@
+import { useState } from "react";
+import { Post } from "../components/api";
+
 export interface ILoginPageProps {}
 
 const LoginPage: React.FC<ILoginPageProps> = (props) => {
+  const [error, setError] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = async (formData: any) => {
+    const response = await Post(formData, "auth/login", true);
+    console.log(response);
+    if (response.token) {
+      setIsLoggedIn(true);
+      localStorage.setItem("auth_token", response.token);
+    } else {
+      setError(true);
+    }
+  };
+
   return (
     <>
-      <form>
+      {error ? <p>Error</p> : null}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          const form = e.target as HTMLFormElement;
+          const formData = new FormData(form);
+
+          const data: Record<string, any> = {};
+          formData.forEach((value, key) => {
+            data[key] = value;
+          });
+
+          login(data);
+        }}
+      >
         <label htmlFor="username">Username</label>
         <input
           type="name"
-          name="password"
-          id="password"
+          name="username"
+          id="username"
           placeholder="Your Username"
         />
         <label htmlFor="password">Passwort</label>
@@ -20,6 +52,7 @@ const LoginPage: React.FC<ILoginPageProps> = (props) => {
         />
         <button>Login</button>
       </form>
+      {isLoggedIn ? <p>Success</p> : null}
     </>
   );
 };
