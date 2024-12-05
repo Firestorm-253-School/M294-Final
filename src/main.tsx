ac2c16ed1d7b7074867cd6d072627a2e3de21e36
Janick-aeschlimann <janick.aeschlimann@bbz-sh.ch>
2024-12-05 10:29:14 +0100

    fix path to loginPage

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
-import LoginPage from "./pages/LoginPage.tsx";
+import LoginPage from "./pages/auth/LoginPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
