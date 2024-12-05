e40eca57657ede5f03ff5e888692b8463c670a8a
Janick-aeschlimann <janick.aeschlimann@bbz-sh.ch>
2024-12-05 14:29:46 +0100

    fix: remove auth-token

--- src/components/home/PostsContainer.tsx
+++ src/components/home/PostsContainer.tsx
@@ -7,37 +7,32 @@ import PostItem from "./PostItem";
 export interface IPostsContainerProps {}
 
 const PostsContainer: React.FC<IPostsContainerProps> = (props) => {
-	const [posts, setPosts] = useState([]);
-	const [isLoading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
-	localStorage.setItem(
-		"auth_token",
-		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjgsImVtYWlsIjoibC56MkBjb20iLCJpYXQiOjE3MzMzOTQ2NjcsImV4cCI6MTczMzQ4MTA2N30.Ng2QN-d8s26pmxI0Du2xPWH9y8UaT2pxqQWer7buqy8"
-	);
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const posts = await ApiGet("posts");
+      setPosts(posts);
+      setLoading(false);
+    })();
+  }, []);
 
-	useEffect(() => {
-		(async () => {
-			setLoading(true);
-			const posts = await ApiGet("posts");
-			setPosts(posts);
-			setLoading(false);
-		})();
-	}, []);
+  if (isLoading) {
+    <h1>Loading...</h1>;
+  }
 
-	if (isLoading) {
-		<h1>Loading...</h1>;
-	}
-
-	return (
-		<>
-			<h1>Post Container</h1>
-			{posts.map((post: Post) => (
-				<div>
-					<PostItem post={post} />
-				</div>
-			))}
-		</>
-	);
+  return (
+    <>
+      <h1>Post Container</h1>
+      {posts.map((post: Post) => (
+        <div>
+          <PostItem post={post} />
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default PostsContainer;
