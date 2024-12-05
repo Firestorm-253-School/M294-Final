24ea488d06ec86ef912d38f64043d6d5898f05aa
Firestorm-253 <laurin.zehnder@hotmail.com>
2024-12-05 12:58:47 +0100

    fix entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import App from "./App.tsx";
+
+import ProtectedRoute from "./components/protectedRoutes/ProtectedRoute.tsx";
 import LoginPage from "./pages/auth/LoginPage.tsx";
 import RegisterPage from "./pages/auth/RegisterPage.tsx";
-import ProtectedRoute from "./components/protectedRoutes/ProtectedRoute.tsx";
+import HomePage from "./pages/home/HomePage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route
+      <Route
           path="/"
           element={
             <ProtectedRoute redirectPath="/login">
-              <App />
+              <HomePage />
             </ProtectedRoute>
           }
         />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
