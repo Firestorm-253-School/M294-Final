e27b2075f13d0a34aaf47cb5950079c9287d500a
GCGgg000 <gcg007@bluewin.ch>
2024-12-09 10:26:34 +0100

    manual merge conflict resolved

--- src/components/home/PostItem.tsx
+++ src/components/home/PostItem.tsx
@@ -7,31 +7,31 @@ import ReactionContainer from "./ReactionContainer";
 import CommentContainer from "./CommentContainer";
 
 export interface IPostItemProps {
-	post: Post;
-	callback_remove: () => void;
-	openPopup: (post: Post) => void;
+  post: Post;
+  callback_remove: () => void;
+  openPopup: (post: Post) => void;
 }
 
 const PostItem: React.FC<IPostItemProps> = (props) => {
-	const { post, callback_remove, openPopup } = props;
-	const [user, setUser] = useState<User | null>(null);
-	const [isLoading, setLoading] = useState(true);
+  const { post, callback_remove, openPopup } = props;
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setLoading] = useState(true);
 
-	useEffect(() => {
-		(async () => {
-			setLoading(true);
-			setUser(await GetUserById(post.user_id));
-			setLoading(false);
-		})();
-	}, []);
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      setUser(await GetUserById(post.user_id));
+      setLoading(false);
+    })();
+  }, []);
 
-	if (isLoading) {
-		return <h1>Loading...</h1>;
-	}
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
-	if (user == null) {
-		return <h1>No user found</h1>;
-	}
+  if (user == null) {
+    return <h1>No user found</h1>;
+  }
 
   return (
     <div className="post-box">
@@ -71,34 +71,19 @@ const PostItem: React.FC<IPostItemProps> = (props) => {
       <div>
         <ReactionContainer post={post} />
         <CommentContainer post={post} />
+        <button
+          onClick={() => {
+            DeletePost(post.id);
+            callback_remove();
+          }}
+        >
+          {"(delete post)"}
+        </button>
+        <button onClick={() => openPopup(post)}>Edit</button>
+        <br />
       </div>
     </div>
   );
-	return (
-		<>
-			<h3>{post.content}</h3>
-			<p>User: {user.username}</p>
-			<p>Created: {post.created_at.toString()}</p>
-			<p>Updated: {post.updated_at.toString()}</p>
-			{post.medialinks.map((medialink: MediaLink) => (
-				<div key={medialink.id}>
-					<p>{medialink.url}</p>
-				</div>
-			))}
-			<ReactionContainer post={post} />
-			<CommentContainer post={post} />
-			<button
-				onClick={() => {
-					DeletePost(post.id);
-					callback_remove();
-				}}
-			>
-				{"(delete post)"}
-			</button>
-			<button onClick={() => openPopup(post)}>Edit</button>
-			<br />
-		</>
-	);
 };
 
 export default PostItem;
