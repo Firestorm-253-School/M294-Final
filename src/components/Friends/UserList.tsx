b045876da7d8ca97cf032f4fa934bbfe9be02b44
GCGgg000 <gcg007@bluewin.ch>
2024-12-18 10:21:26 +0100

    Hide add friend button in UserCard within UserList component

--- src/components/Friends/UserList.tsx
+++ src/components/Friends/UserList.tsx
@@ -20,7 +20,7 @@ const UserList: React.FC<UserListProps> = ({ users, refresh }) => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {users.map((user) => (
         <div key={user.profileId} className="card w-full bg-base-100 shadow-md p-4">
-          <UserCard profile={user} refresh={refresh} />
+          <UserCard profile={user} refresh={refresh} showAddButton={false} />
         </div>
       ))}
     </div>
