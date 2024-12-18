5942a2a6803b076efeebc5f816a5185e3af3a2bd
Janick-aeschlimann <janick.aeschlimann@bbz-sh.ch>
2024-12-18 07:18:42 +0100

    sendMessageTest

--- src/components/sockets/SocketContext.tsx
+++ src/components/sockets/SocketContext.tsx
@@ -10,7 +10,7 @@ export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({
   const [isConnected, setIsConnected] = useState(false);
 
   useEffect(() => {
-    const newSocket = io("http://192.168.100.40:3000", {
+    const newSocket = io("http://localhost:3000", {
       auth: {
         token: localStorage.getItem("auth_token"),
       },
@@ -19,6 +19,10 @@ export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({
     function onConnect() {
       setIsConnected(true);
       console.log("Connected to server");
+      newSocket.emit("chat_message", {
+        userId: 11,
+        message: "Hello from the client!",
+      });
     }
 
     function onDisconnect() {
